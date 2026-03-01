0b4f85ed234aa7540611abe41eba098e5d838b58
Md. Robiul Hossain <robiulrana2k16@gmail.com>
2026-03-01 21:06:18 +0600

    files are added

---
+++ tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to the Django")
+
+def show_task(request):
+    return HttpResponse("<h1>This is our task page</h1>")
---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# from django.https import HttpResponse
+# # Create your views here.
+
+# def home(request):
+#     return HttpResponse("Welcome to the Django")
+
